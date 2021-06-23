21e03f3cf481db7da149184db5f6d82972afdf9e
Alex <alexmhh1234@gmail.com>
2021-06-23 23:34:06 +0300

    refactoring down and still optimizing

--- main.py
+++ main.py
@@ -1,50 +1,61 @@
 from PIL import ImageGrab, ImageOps
 import numpy
 import keyboard
-import time
+import win32api
 
-previous_value = 0
+initial_state = 0
 ticks = 0
 difficulty_modifier = 1
-x1Coordinate, y1Coordinate, x1Offset, y1Offset = 510, 292, 1, 1
-x2Coordinate, y2Coordinate, x2Offset, y2Offset = 677, 266, 1, 1
-x3Coordinate, y3Coordinate, x3Offset, y3Offset = 490, 292, 1, 1
+x1Coordinate, y1Coordinate, x1Offset, y1Offset = 510, 292, 10, 1
+x3Coordinate, y3Coordinate, x3Offset, y3Offset = 490, 292, 10, 1
+birdXCoordinate, birdYCoordinate, xOffset, yOffset = 460, 261, 10, 1
+farX, farY, farXOffset, farYOffset = 535, 292, 10, 1
+
+
+def turn_box_into_array_sum(box):
+    image = ImageGrab.grab(box)
+    grayImage = ImageOps.grayscale(image)
+    array = numpy.array(grayImage.getcolors())
+    value = array.sum()
+    return value
+
+
+def multiply_coordinate(coordinate, modifier):
+    coordinate *= modifier
+    coordinate = numpy.ceil(coordinate)
+    return coordinate
+
+
+def reset_values():
+    return 0, 0, 1, 510, 490
+
+
 while True:
     if keyboard.is_pressed('q'):
         print("Stopping game!")
         break
+    if win32api.GetKeyState(0x01) < 0:
+        print("Game restart!")
+        initial_state, ticks, difficulty_modifier, x1Coordinate, x3Coordinate = reset_values()
     box1 = (x1Coordinate, y1Coordinate, x1Coordinate + x1Offset, y1Coordinate + y1Offset)
     box3 = (x3Coordinate, y3Coordinate, x3Coordinate + x3Offset, y3Coordinate + y3Offset)
-    image = ImageGrab.grab(box1)
-    _image = ImageGrab.grab(box3)
-    grayImage = ImageOps.grayscale(image)
-    array = numpy.array(grayImage.getcolors())
-    value = array.sum()
-    grayImage = ImageOps.grayscale(_image)
-    array = numpy.array(grayImage.getcolors())
-    value_ = array.sum()
-    if previous_value != 0:
-        if value != previous_value:
+    boxBird = (birdXCoordinate, birdYCoordinate, birdXCoordinate + xOffset, birdYCoordinate + yOffset)
+    value = turn_box_into_array_sum(box1)
+    value_ = turn_box_into_array_sum(box3)
+    valueBird = turn_box_into_array_sum(boxBird)
+    if initial_state != 0:
+        if value != initial_state:
             keyboard.press('space')
-            previous_value = value
-        elif value_ != previous_value:
+        elif value_ != initial_state:
             keyboard.press('space')
-            previous_value = value_
+        if valueBird != initial_state:
+            keyboard.press('down')
     else:
-        previous_value = value
-    box2 = (x2Coordinate, y2Coordinate, x2Coordinate+x2Offset, y2Coordinate+y2Offset)
-    image = ImageGrab.grab(box2)
-    grayImage = ImageOps.grayscale(image)
-    array = numpy.array(grayImage.getcolors())
-    value = array.sum()
-    # if value == 84:
-    #     time.sleep(2)
-    #     keyboard.press('space')
+        initial_state = value
     if ticks == 100:
         difficulty_modifier *= 1.01
-        print(difficulty_modifier)
-        x1Coordinate *= difficulty_modifier
-        x1Coordinate = numpy.ceil(x1Coordinate)
+        x1Coordinate = multiply_coordinate(x1Coordinate, difficulty_modifier)
+        x3Coordinate = multiply_coordinate(x3Coordinate, difficulty_modifier)
         ticks = 0
     else:
-        ticks +=1
+        ticks += 1
