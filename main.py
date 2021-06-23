a41cb7036438e9f9e3850b53a5b55d81c2ad88ea
Alex <alexmhh1234@gmail.com>
2021-06-23 19:06:43 +0300

    a tiny more complex

--- main.py
+++ main.py
@@ -4,24 +4,41 @@ import keyboard
 import time
 
 previous_value = 0
-start_time = time.time()
+ticks = 0
 difficulty_modifier = 1
-xCoordinate, yCoordinate, xOffset, yOffset = 510, 292, 1, 1
+x1Coordinate, y1Coordinate, x1Offset, y1Offset = 510, 292, 1, 1
+x2Coordinate, y2Coordinate, x2Offset, y2Offset = 677, 266, 1, 1
+x3Coordinate, y3Coordinate, x3Offset, y3Offset = 490, 292, 1, 1
 while True:
     if keyboard.is_pressed('q'):
         print("Stopping game!")
         break
-    box1 = (xCoordinate, yCoordinate, xCoordinate+xOffset, yCoordinate+yOffset)
+    box1 = (x1Coordinate, y1Coordinate, x1Coordinate + x1Offset, y1Coordinate + y1Offset)
+    box3 = (x3Coordinate, y3Coordinate, x3Coordinate + x3Offset, y3Coordinate + y3Offset)
     image = ImageGrab.grab(box1)
+    _image = ImageGrab.grab(box3)
     grayImage = ImageOps.grayscale(image)
     array = numpy.array(grayImage.getcolors())
     value = array.sum()
-    print(value)
-    if value != previous_value and previous_value != 0:
+    grayImage = ImageOps.grayscale(_image)
+    array = numpy.array(grayImage.getcolors())
+    value_ = array.sum()
+    if ((value != previous_value and value_ == previous_value) or (value == previous_value and value_ != previous_value)) and previous_value != 0:
         keyboard.press('space')
-        pass
     previous_value = value
-    if numpy.floor(time.time() - start_time+1) % 3 == 0:
-        difficulty_modifier *= 1.05
-        xCoordinate *= difficulty_modifier
-        xCoordinate = numpy.ceil(xCoordinate)
+    box2 = (x2Coordinate, y2Coordinate, x2Coordinate+x2Offset, y2Coordinate+y2Offset)
+    image = ImageGrab.grab(box2)
+    grayImage = ImageOps.grayscale(image)
+    array = numpy.array(grayImage.getcolors())
+    value = array.sum()
+    # if value == 84:
+    #     time.sleep(2)
+    #     keyboard.press('space')
+    if ticks == 100:
+        difficulty_modifier *= 1.01
+        print(difficulty_modifier)
+        x1Coordinate *= difficulty_modifier
+        x1Coordinate = numpy.ceil(x1Coordinate)
+        ticks = 0
+    else:
+        ticks += 1
